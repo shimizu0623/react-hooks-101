dc476c31e27826aa2898fb073d9b820cdd6e7989
shimizu <github89590207+shimizu0623@users.noreply.github.com>
2021-11-03 17:21:59 +0900

    implement callbacks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const App= props => {
   const [state, setState] = useState(props)
   const {name, price} = state
 
+  useEffect(()=>{
+    console.log('useEffecttt')
+  }, [])
+
+  useEffect(()=>{
+    console.log('This callback is for name only')
+  }, [name])
+
+
   return (
     <>
-     <p>{name}は￥{price}</p>
+    <p>{name}は￥{price}です</p>
      <button onClick={()=>setState({...state, price: price + 1})}>+1</button>
      <button onClick={()=>setState({...state, price: price - 1})}>-1</button>
      <button onClick={()=>setState(props)}>Reset</button>
