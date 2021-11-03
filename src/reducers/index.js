86b1f789a7f5967638993b196dc2a8a59935a658
shimizu <github89590207+shimizu0623@users.noreply.github.com>
2021-11-03 23:30:05 +0900

    create reducer for events

---
+++ src/reducers/index.js
@@ -0,0 +1,58 @@
+// action = {
+//     type: 'CREATE_EVENT',
+//     title: 'おしらせ',
+//     body: '2020年にオリンピックを開催します！つきましては...'
+// }
+
+// # before
+// state = []
+// # after
+// state = [
+//     {
+//         id: 1,
+//         title: 'おしらせ',
+//         body: '2020年にオリンピックを開催します！つきましては...'
+//     }
+// ]
+
+
+// # before
+// state = [
+//     { id: 1, title: 'タイトル1', body: 'ボディ1'},
+//     { id: 2, title: 'タイトル2', body: 'ボディ2'},
+//     { id: 3, title: 'タイトル3', body: 'ボディ3'},
+// ]
+
+// # after
+// state = [
+//     { id: 1, title: 'タイトル1', body: 'ボディ1'},
+//     { id: 2, title: 'タイトル2', body: 'ボディ2'},
+//     { id: 3, title: 'タイトル3', body: 'ボディ3'},
+//     {
+//       id: 4,
+//       title: 'おしらせ',
+//       body: '2020年にオリンピックを開催します！つきましては...'
+//     }
+// ]
+
+
+
+
+const events = (state = [], action)=>{
+    switch(action.type){
+        case 'CREATE_EVENT':
+         const event = { title: action.title, body: action.body}
+         const length = state.length
+         const id =length === 0 ? 1 : state[length - 1].id + 1
+         return [...state, { id, ...event}]
+        case 'DELETE_EVENT':
+         return state
+        case 'DELETE_ALL_EVENTS':
+         return []
+        default:
+         return state
+             
+    }
+}
+
+export default events
