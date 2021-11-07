5c1da7c7b939a1795ed4984197adde7f83fb3a06
shimizu <github89590207+shimizu0623@users.noreply.github.com>
2021-11-07 14:29:32 +0900

    create oparation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -2,10 +2,13 @@ import React, { useContext, useState } from 'react';
 
 import {
   CREATE_EVENT,
-  DELETE_ALL_EVENTS
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS
 }from '../actions'
 
 import AppContext from '../contexts/AppContext';
+import {timeCurrentIso8601} from '../utils';
 
 // import reducer from '../reducers'
 
@@ -29,6 +32,12 @@ const EventForm =()=>{
       //   title,
       //   body
       // }
+
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: 'イベントを作成しました',
+        operatedAt: timeCurrentIso8601()
+      })
   
       setTitle('')
       setBody('')
@@ -38,8 +47,14 @@ const EventForm =()=>{
     const deleteAllEvents = e =>{
       e.preventDefault()
       const result = window.confirm("すべて削除しますか？")
-      if (result) dispatch({type: DELETE_ALL_EVENTS})
-  
+      if (result) {
+        dispatch({type: DELETE_ALL_EVENTS})
+        dispatch({
+          type: ADD_OPERATION_LOG,
+          description: '全てのイベントを削除しました',
+          operatedAt: timeCurrentIso8601()
+        })
+      }
     }
   
     const unCreatable = title ==='' || body ===''
